33324681fdf8262e4b642276a057ddb8f98b46c3
ridwanNurfauzi <mrnhp123@gmail.com>
2024-04-06 09:50:23 +0700

    added some functions

--- src/controllers/user/productsController.ts
+++ src/controllers/user/productsController.ts
@@ -11,19 +11,20 @@ const index = async (req: Request, res: Response) => {
     try {
         const data = await Product.findAll({
             attributes: {
-                exclude: ['id', 'seller_id']
+                exclude: ['id', 'description']
             },
             include: [
                 {
                     association: new HasMany(Product, Product_image, {
-                        foreignKey: { name: 'product_id' },
+                        foreignKey: 'product_id',
                         as: 'product_images'
                     }),
                     attributes: ['filename']
                 },
                 {
                     association: new HasOne(Product, Seller, {
-                        foreignKey: { name: 'id' },
+                        sourceKey: 'seller_id',
+                        foreignKey: 'id',
                         as: 'seller'
                     }),
                     attributes: {
@@ -69,6 +70,203 @@ const index = async (req: Request, res: Response) => {
     }
 };
 
+
+const latest = async (req: Request, res: Response) => {
+    try {
+        const response = await Product.findAll({
+            attributes: {
+                exclude: ['id', 'description']
+            },
+            include: [
+                {
+                    association: new HasMany(Product, Product_image, {
+                        foreignKey: 'product_id',
+                        as: 'product_images'
+                    }),
+                    attributes: ['filename']
+                },
+                {
+                    association: new HasOne(Product, Seller, {
+                        sourceKey: 'seller_id',
+                        foreignKey: 'id',
+                        as: 'seller'
+                    }),
+                    attributes: {
+                        exclude: ['id', 'password', 'createdAt', 'updatedAt']
+                    }
+                },
+                {
+                    association: 'categories',
+                    attributes: {
+                        exclude: ['id', 'createdAt', 'updatedAt']
+                    }
+                },
+                {
+                    association: new HasMany(Product, Review, {
+                        foreignKey: { name: 'product_id' },
+                        as: 'reviews'
+                    }),
+                    include: [
+                        {
+                            association: new HasOne(Review, User, {
+                                as: 'user',
+                                foreignKey: 'id',
+                                sourceKey: 'user_id'
+                            }),
+                            attributes: {
+                                exclude: ['id', 'password']
+                            }
+                        }
+                    ]
+                }
+            ]
+        });
+
+        const data = response.sort((a, b) => new Date(a.createdAt ?? '').getTime() - new Date(b.createdAt ?? '').getTime()).reverse();
+
+        res.send({
+            success: true,
+            data
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success: false
+        });
+    }
+};
+
+const earliest = async (req: Request, res: Response) => {
+    try {
+        const response = await Product.findAll({
+            attributes: {
+                exclude: ['id', 'description']
+            },
+            include: [
+                {
+                    association: new HasMany(Product, Product_image, {
+                        foreignKey: 'product_id',
+                        as: 'product_images'
+                    }),
+                    attributes: ['filename']
+                },
+                {
+                    association: new HasOne(Product, Seller, {
+                        sourceKey: 'seller_id',
+                        foreignKey: 'id',
+                        as: 'seller'
+                    }),
+                    attributes: {
+                        exclude: ['id', 'password', 'createdAt', 'updatedAt']
+                    }
+                },
+                {
+                    association: 'categories',
+                    attributes: {
+                        exclude: ['id', 'createdAt', 'updatedAt']
+                    }
+                },
+                {
+                    association: new HasMany(Product, Review, {
+                        foreignKey: { name: 'product_id' },
+                        as: 'reviews'
+                    }),
+                    include: [
+                        {
+                            association: new HasOne(Review, User, {
+                                as: 'user',
+                                foreignKey: 'id',
+                                sourceKey: 'user_id'
+                            }),
+                            attributes: {
+                                exclude: ['id', 'password']
+                            }
+                        }
+                    ]
+                }
+            ]
+        });
+
+        const data = response.sort((a, b) => new Date(a.createdAt ?? '').getTime() - new Date(b.createdAt ?? '').getTime());
+
+        res.send({
+            success: true,
+            data
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success: false
+        });
+    }
+};
+
+const popular = async (req: Request, res: Response) => {
+    try {
+        const response = await Product.findAll({
+            attributes: {
+                exclude: ['id', 'description']
+            },
+            include: [
+                {
+                    association: new HasMany(Product, Product_image, {
+                        foreignKey: 'product_id',
+                        as: 'product_images'
+                    }),
+                    attributes: ['filename']
+                },
+                {
+                    association: new HasOne(Product, Seller, {
+                        sourceKey: 'seller_id',
+                        foreignKey: 'id',
+                        as: 'seller'
+                    }),
+                    attributes: {
+                        exclude: ['id', 'password', 'createdAt', 'updatedAt']
+                    }
+                },
+                {
+                    association: 'categories',
+                    attributes: {
+                        exclude: ['id', 'createdAt', 'updatedAt']
+                    }
+                },
+                {
+                    association: new HasMany(Product, Review, {
+                        foreignKey: { name: 'product_id' },
+                        as: 'reviews'
+                    }),
+                    include: [
+                        {
+                            association: new HasOne(Review, User, {
+                                as: 'user',
+                                foreignKey: 'id',
+                                sourceKey: 'user_id'
+                            }),
+                            attributes: {
+                                exclude: ['id', 'password']
+                            }
+                        }
+                    ]
+                }
+            ]
+        });
+
+        const data = response.sort((a, b) => a.sold - b.sold).reverse();
+
+        res.send({
+            success: true,
+            data
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success: false
+        });
+    }
+};
+
+
 const getBySlug = async (req: Request, res: Response) => {
     try {
         const data = await Product.findOne({
@@ -88,7 +286,8 @@ const getBySlug = async (req: Request, res: Response) => {
                 },
                 {
                     association: new HasOne(Product, Seller, {
-                        foreignKey: { name: 'id' },
+                        sourceKey: 'seller_id',
+                        foreignKey: 'id',
                         as: 'seller'
                     }),
                     attributes: {
@@ -137,5 +336,8 @@ const getBySlug = async (req: Request, res: Response) => {
 
 export {
     index,
+    latest,
+    earliest,
+    popular,
     getBySlug
 };
