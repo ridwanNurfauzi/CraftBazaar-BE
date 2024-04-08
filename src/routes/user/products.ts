a584cd64026d54781afbb58df290f74933e8f620
ridwanNurfauzi <mrnhp123@gmail.com>
2024-04-08 10:07:22 +0700

    added some routes in user products route

--- src/routes/user/products.ts
+++ src/routes/user/products.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import * as products from "../../controllers/user/productsController";
-import { verifyToken } from "../../middleware/user/verify";
+import { verifyToken, getTokenDecode } from "../../middleware/user/verify";
 
 const router = Router();
 
@@ -9,6 +9,8 @@ router.get('/latest', products.latest);
 router.get('/earliest', products.earliest);
 router.get('/popular', products.popular);
 
-router.get('/slug/:slug', products.getBySlug);
+router.get('/slug/:slug', getTokenDecode, products.getBySlug);
+router.delete('/reviews/:id', verifyToken, products.removePersonalReview);
+router.post('/reviews/post', verifyToken, products.addReview);
 
 export default router;
