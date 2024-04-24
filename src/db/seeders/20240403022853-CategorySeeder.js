7920422f81e585ed6ac82d97c8af2f145a33c950
ridwanNurfauzi <mrnhp123@gmail.com>
2024-04-24 16:28:42 +0700

    edited seeders

--- src/db/seeders/20240403022853-CategorySeeder.js
+++ src/db/seeders/20240403022853-CategorySeeder.js
@@ -1,15 +1,51 @@
 'use strict';
 
 const data = [
-  { name: 'Bahan Bekas' },
-  { name: 'Kertas' },
-  { name: 'Sedotan' },
-  { name: 'Tanah Liat' },
-  { name: 'Origami' },
-  { name: 'Anyaman' },
-  { name: 'Stik Es' },
-  { name: 'Mainan' },
-  { name: 'Hiasan' }
+  {
+    name: 'Bahan Bekas',
+    createdAt: new Date(1400000000004),
+    updatedAt: new Date(1400000000004)
+  },
+  {
+    name: 'Kertas',
+    createdAt: new Date(1401000000004),
+    updatedAt: new Date(1401000000004)
+  },
+  {
+    name: 'Sedotan',
+    createdAt: new Date(1402000000004),
+    updatedAt: new Date(1402007000004)
+  },
+  {
+    name: 'Tanah Liat',
+    createdAt: new Date(1403000000004),
+    updatedAt: new Date(1403000000004)
+  },
+  {
+    name: 'Origami',
+    createdAt: new Date(1404000000004),
+    updatedAt: new Date(1404000000004)
+  },
+  {
+    name: 'Anyaman',
+    createdAt: new Date(1405000000004),
+    updatedAt: new Date(1405900000004)
+  },
+  {
+    name: 'Stik Es',
+    createdAt: new Date(1406000000004),
+    updatedAt: new Date(1406000000004)
+  },
+  {
+    name: 'Mainan',
+    createdAt: new Date(1407000000004),
+    updatedAt: new Date(1407800000004)
+  },
+  {
+    name: 'Hiasan',
+    createdAt: new Date(1408000000004),
+    updatedAt: new Date(1408020000004)
+  }
 ];
 
 /** @type {import('sequelize-cli').Migration} */
--- src/db/seeders/20240403233057-ReviewSeeder.js
+++ src/db/seeders/20240403233057-ReviewSeeder.js
@@ -6,98 +6,184 @@ const data = [
     text: 'Bagus',
     product_id: 1,
     user_id: 3,
-    createdAt: new Date(1501000000004)
+    createdAt: new Date(1501000000004),
+    updatedAt: new Date(1501000000004)
   },
   {
     rating: 4,
     text: 'Sangat Bagus',
     product_id: 3,
     user_id: 3,
-    createdAt: new Date(1502000000004)
+    createdAt: new Date(1502000000004),
+    updatedAt: new Date(1502000000004)
   },
   {
     rating: 3,
     text: 'Lumayan',
     product_id: 1,
     user_id: 1,
-    createdAt: new Date(1503000000004)
+    createdAt: new Date(1503000000004),
+    updatedAt: new Date(1503000000004)
   },
   {
     rating: 2,
     text: 'Mudah rusak.',
     product_id: 4,
     user_id: 1,
-    createdAt: new Date(1504000000004)
+    createdAt: new Date(1504000000004),
+    updatedAt: new Date(1504000000004)
   },
   {
     rating: 4,
     text: 'Warnanya kurang menarik.',
     product_id: 9,
     user_id: 2,
-    createdAt: new Date(1505000000004)
-  },
-  {
-    rating: 4,
-    text: 'tidak buruk',
-    product_id: 1,
-    user_id: 1,
-    createdAt: new Date(1506000000004)
+    createdAt: new Date(1505000000004),
+    updatedAt: new Date(1505000000004)
   },
   {
     rating: 5,
     text: 'sangat bagus',
     product_id: 1,
     user_id: 2,
-    createdAt: new Date(1507000000004)
+    createdAt: new Date(1507000000004),
+    updatedAt: new Date(1507000000004)
   },
   {
     rating: 2,
     text: 'Mudah sobek',
     product_id: 1,
-    user_id: 3,
-    createdAt: new Date(1508000000004)
+    user_id: 4,
+    createdAt: new Date(1508000000004),
+    updatedAt: new Date(1508000000004)
   },
   {
     rating: 2,
     text: 'Mudah pecah',
     product_id: 15,
     user_id: 3,
-    createdAt: new Date(1509000000004)
+    createdAt: new Date(1509000000004),
+    updatedAt: new Date(1509000000004)
   },
   {
-    rating: 2,
+    rating: 1,
+    text: 'Mudah pecah',
+    product_id: 13,
+    user_id: 8,
+    createdAt: new Date(1509000000004),
+    updatedAt: new Date(1509000000004)
+  },
+  {
+    rating: 3,
     text: 'Warnanya kurang menarik',
     product_id: 2,
     user_id: 2,
-    createdAt: new Date(1510000000004)
+    createdAt: new Date(1510000000004),
+    updatedAt: new Date(1510000000004)
   },
   {
     rating: 5,
     text: 'Sempurna',
     product_id: 4,
     user_id: 4,
-    createdAt: new Date(1511000000004)
+    createdAt: new Date(1511000000004),
+    updatedAt: new Date(1511000000004)
   },
   {
     rating: 4,
     text: 'Ukuran sesuai dengan buku',
     product_id: 7,
     user_id: 3,
-    createdAt: new Date(1512000000004)
+    createdAt: new Date(1512000000004),
+    updatedAt: new Date(1512000000004)
   },
   {
     rating: 2,
     text: 'Mudah sobek',
     product_id: 3,
     user_id: 1,
-    createdAt: new Date(1513000000004)
+    createdAt: new Date(1513000000004),
+    updatedAt: new Date(1513000000004)
   },
   {
     rating: 2,
     text: 'Mudah rusak',
     product_id: 10,
     user_id: 2,
-    createdAt: new Date(1516000000004)
+    createdAt: new Date(1516000000004),
+    updatedAt: new Date(1516000000004)
+  },
+  {
+    rating: 4,
+    text: 'Bagus',
+    product_id: 8,
+    user_id: 2,
+    createdAt: new Date(1516000000004),
+    updatedAt: new Date(1516000000004)
+  },
+  {
+    rating: 5,
+    text: 'Luar biasa.',
+    product_id: 8,
+    user_id: 1,
+    createdAt: new Date(1526000000004),
+    updatedAt: new Date(1526000000004)
+  },
+  {
+    rating: 5,
+    text: 'Nyaman saat digunakan.',
+    product_id: 11,
+    user_id: 1,
+    createdAt: new Date(1522000000004),
+    updatedAt: new Date(1522000000004)
+  },
+  {
+    rating: 3,
+    text: 'Lumayan.',
+    product_id: 11,
+    user_id: 5,
+    createdAt: new Date(1526000000004),
+    updatedAt: new Date(1526000000004)
+  },
+  {
+    rating: 5,
+    text: 'Warnanya menarik.',
+    product_id: 6,
+    user_id: 1,
+    createdAt: new Date(1526000000004),
+    updatedAt: new Date(1526000000004)
+  },
+  {
+    rating: 4,
+    text: 'Bentuknya menarik.',
+    product_id: 6,
+    user_id: 2,
+    createdAt: new Date(1526000000004),
+    updatedAt: new Date(1526000000004)
+  },
+  {
+    rating: 4,
+    text: 'Bagus cocok untuk dekorasi rumah..',
+    product_id: 6,
+    user_id: 6,
+    createdAt: new Date(1526000000004),
+    updatedAt: new Date(1526000000004)
+  },
+  {
+    rating: 4,
+    text: 'menarik',
+    product_id: 6,
+    user_id: 7,
+    createdAt: new Date(1526000000004),
+    updatedAt: new Date(1526000000004)
+  },
+  {
+    rating: 4,
+    text: 'menarik',
+    product_id: 5,
+    user_id: 7,
+    createdAt: new Date(1516000000004),
+    updatedAt: new Date(1516000000004)
   },
 ];
 
